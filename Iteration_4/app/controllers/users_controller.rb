5f84408a2afb12e1e800fe7fa9f22c745849715a
Aniket Sanjiv Bonde <aniketbonde7@gmail.com>
2017-05-03 00:04:58 -0500

    added flash (admin id)
    added flash that shows admin id after account creation

--- Iteration_4/app/controllers/users_controller.rb
+++ Iteration_4/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      #flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Admin ID: #{@user.id} (Give this to users for them to participate in polls)"
       redirect_to @user
     else
       render 'new'
